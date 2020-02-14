f72c971e06eedd9c936e73249c3605736610cae5
Igor <igor@guaiuba-006888.local>
2020-02-14 13:52:48 -0300

    [#1] Pushing code

---
+++ config/dao/movies_dao.go
@@ -0,0 +1,55 @@
+package dao
+
+import (
+	"log"
+
+	. "../../models"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type MovieDAO struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+const (
+	COLLECTION = "movies"
+)
+
+func (m *MovieDAO) Connect() {
+	session, err := mgo.Dial(m.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(m.Database)
+}
+
+func (m *MovieDAO) GetAll() ([]Movie, error) {
+	var movies []Movie
+	err := db.C(COLLECTION).Find(bson.M{}).All(&movies)
+	return movies, err
+}
+
+func (m *MovieDAO) GetByID(id string) (Movie, error) {
+	var movie Movie
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&movie)
+	return movie, err
+}
+
+func (m *MovieDAO) Create(movie Movie) error {
+	err := db.C(COLLECTION).Insert(&movie)
+	return err
+}
+
+func (m *MovieDAO) Delete(id string) error {
+	err := db.C(COLLECTION).RemoveId(bson.ObjectIdHex(id))
+	return err
+}
+
+func (m *MovieDAO) Update(id string, movie Movie) error {
+	err := db.C(COLLECTION).UpdateId(bson.ObjectIdHex(id), &movie)
+	return err
+}
